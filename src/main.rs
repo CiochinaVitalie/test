9f339be4b3c0df84c837561d479c967c11788779
Vitalie Ciochina <ciochina.v@ajax.systems>
2024-10-04 00:00:28 +0300

    Delete bme680

--- src/main.rs
+++ src/main.rs
@@ -20,7 +20,6 @@ use panic_probe as _;
 use embedded_hal::blocking::i2c::{Read, Write};
 
 use rp_pico::hal::fugit::RateExtU32;
-use bme680::*;
 
 use rp_pico::hal::{
     clocks::{init_clocks_and_plls, Clock},
@@ -75,9 +74,6 @@ fn main() -> ! {
         // Configure two pins as being I²C, not GPIO
     let sda: Pin<_, FunctionI2C, _> = pins.gpio18.reconfigure();
     let scl: Pin<_, FunctionI2C, _> = pins.gpio19.reconfigure();
-
-    let sda0: Pin<_, FunctionI2C, _> = pins.gpio16.reconfigure();
-    let scl0: Pin<_, FunctionI2C, _> = pins.gpio17.reconfigure();
    
 
     let i2c =  rp_pico::hal::I2C::i2c1(
@@ -88,60 +84,21 @@ fn main() -> ! {
         &mut pac.RESETS,
         &clocks.system_clock,
     );
-    let i2c0 =  rp_pico::hal::I2C::i2c0(
-        pac.I2C0,
-        sda0,
-        scl0, // Try `not_an_scl_pin` here
-        100.kHz(),
-        &mut pac.RESETS,
-        &clocks.system_clock,
-    );
-   
     
     let en_pin = pins.gpio20.into_push_pull_output_in_state(PinState::Low);
     let nrdy_pin =pins.gpio21.into_pull_up_input();
 
     //let mut  test:Option<Sunrise<rp_pico::hal::I2C<pac::I2C1, (Pin<rp_pico::hal::gpio::bank0::Gpio18, rp_pico::hal::gpio::FunctionI2c, rp_pico::hal::gpio::PullUp>, Pin<rp_pico::hal::gpio::bank0::Gpio19, rp_pico::hal::gpio::FunctionI2c, rp_pico::hal::gpio::PullUp>)>, cortex_m::delay::Delay, Pin<rp_pico::hal::gpio::bank0::Gpio20, rp_pico::hal::gpio::FunctionSio<rp_pico::hal::gpio::SioOutput>, rp_pico::hal::gpio::PullDown>, rp_pico::hal::gpio::AsInputPin<rp_pico::hal::gpio::bank0::Gpio21, rp_pico::hal::gpio::FunctionNull, rp_pico::hal::gpio::PullDown>> > = None;
     let mut  test:Option<Sunrise<I2C<I2C1, (Pin<Gpio18, FunctionI2c, PullUp>, Pin<Gpio19, FunctionI2c, PullUp>)>, Delay, Pin<Gpio20, FunctionSio<SioOutput>, PullDown>, Pin<Gpio21, FunctionSio<SioInput>, PullUp>>> = None;
-    let mut test1:Option<Bme680<I2C<I2C0, (Pin<Gpio16, FunctionI2c, PullUp>, Pin<Gpio17, FunctionI2c, PullUp>)>, Delay>> = None;
+
     unsafe {
         if let Some(delay) = GLOBAL_DELAY.as_mut() {
             test = Some(Sunrise::new(i2c, delay, en_pin, nrdy_pin));
         }
     }
-    unsafe {
-        if let Some(delay) = GLOBAL_DELAY.as_mut() {
-            test1 = Some( Bme680::init(i2c0, delay, I2CAddress::Secondary).unwrap());
-        }
-    }
-    let mut sensor_CO2 = test.take().unwrap();
-    let mut bme680 = test1.take().unwrap();
-    let settings = SettingsBuilder::new()
-    .with_humidity_oversampling(OversamplingSetting::OS2x)
-    .with_pressure_oversampling(OversamplingSetting::OS4x)
-    .with_temperature_oversampling(OversamplingSetting::OS8x)
-    .with_temperature_filter(IIRFilterSize::Size3)
-    .with_gas_measurement(Duration::from_millis(1500), 320, 25)
-    .with_temperature_offset(-2.2)
-    .with_run_gas(true)
-    .build();
-    let profile_dur = bme680.get_profile_dur(&settings.0).unwrap();
-    info!("Profile duration {:?}", profile_dur);
-    info!("Setting sensor settings");
 
-    unsafe {
-        if let Some(delay) = GLOBAL_DELAY.as_mut() {
-            bme680.set_sensor_settings(delay, settings).unwrap();
-        }
-    }
-    info!("Setting forced power modes");
-    unsafe {
-        if let Some(delay) = GLOBAL_DELAY.as_mut() {
-            bme680.set_sensor_mode(delay, PowerMode::ForcedMode).unwrap();
-        }
-    }
-    let sensor_settings = bme680.get_sensor_settings(settings.1);
-  
+    let mut sensor_CO2 = test.take().unwrap();
+
     sensor_CO2.init(false,true,false).unwrap();
 
     
@@ -152,20 +109,6 @@ fn main() -> ! {
     
     let sensor_data = ((data[6] as u16) << 8) | (data[7] as u16);
     info!("Read CO2 data is {}",sensor_data);
-    unsafe {
-        if let Some(delay) = GLOBAL_DELAY.as_mut() {
-            bme680.set_sensor_mode(delay, PowerMode::ForcedMode).unwrap();
-        }
-    }
-    unsafe {
-        if let Some(delay) = GLOBAL_DELAY.as_mut() {
-            let (data, _state) = bme680.get_sensor_data(delay).unwrap();
-            info!("Temperature {}°C", data.temperature_celsius());
-            info!("Pressure {}hPa", data.pressure_hpa());
-            info!("Humidity {}%", data.humidity_percent());
-            info!("Gas Resistence {}Ω", data.gas_resistance_ohm());
-        }
-    }
     
     unsafe {
         if let Some(delay) = GLOBAL_DELAY.as_mut() {
